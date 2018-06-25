b38a6d76296f40c64254defada318701d761947c
OleHolmNielsen <Ole.H.Nielsen@fysik.dtu.dk>
2018-06-25 15:13:25 +0200

    MoS2-benchmark

---
+++ benchmarks/MoS2-benchmark.py
@@ -0,0 +1,33 @@
+from math import pi
+import numpy as np
+from ase.build import mx2
+from gpaw import GPAW, PW, Davidson
+
+# Create tube of MoS2:
+atoms = mx2('MoS2', size=(3, 2, 1))
+atoms.cell[1, 0] = 0
+atoms = atoms.repeat((1, 10, 1))
+p = atoms.positions
+p2 = p.copy()
+L = atoms.cell[1, 1]
+r0 = L / (2 * pi)
+angle = p[:, 1] / L * 2 * pi
+p2[:, 1] = (r0 + p[:, 2]) * np.cos(angle)
+p2[:, 2] = (r0 + p[:, 2]) * np.sin(angle)
+atoms.positions = p2
+atoms.cell = [atoms.cell[0, 0], 0, 0]
+
+# setup calculator
+ecut = 800
+kpoints = (4, 1, 1)
+atoms.center(vacuum=6, axis=[1, 2])
+atoms.pbc = True
+tag = 'MoS2-benchmark'
+atoms.calc = GPAW(mode=PW(ecut),
+                  eigensolver=Davidson(2),
+                  xc='PBE',
+                  txt=tag + '.txt',
+                  kpts={'size': kpoints},
+                  nbands='120%',
+                  parallel={'augment_grids': True})
+forces = atoms.get_forces()
---
+++ benchmarks/MoS2-benchmark.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=MoS2-benchmark
+#SBATCH --mail-type=START,END
+#SBATCH --partition=xeon24
+#SBATCH --output=%x-%j.out
+#SBATCH --time=6:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=24
+#SBATCH --ntasks-per-node=24
+#SBATCH --mem=250G
+
+# This is a Slurm batch job script for the MoS2-benchmark.py benchmark.
+# It is assumed that a GPAW 1.4 software module can be loaded.
+
+echo Hostname: `hostname`
+echo CPU type:
+lscpu
+
+module purge
+module load GPAW/1.4.0-foss-2018a-Python-3.6.4
+module list
+
+mpiexec gpaw-python MoS2-benchmark.py
